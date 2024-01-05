51857fccf6e47a4928b1da1aa7fa5112f1208410
soheil mahmoodi <soheilmahmoodisr47@gmail.com>
2024-01-05 20:09:51 +0330

    adding POJOs. closes #2.

---
+++ src/main/java/org/example/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package org.example.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/org/example/petclinic/model/Person.java
@@ -0,0 +1,22 @@
+package org.example.petclinic.model;
+
+public class Person {
+    private String firsName;
+    private String lastName;
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
