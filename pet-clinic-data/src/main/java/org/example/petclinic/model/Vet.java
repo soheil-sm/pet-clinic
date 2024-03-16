929c8d802673c87b000312f7057995b2170a99b8
soheil mahmoodi <soheilmahmoodisr47@gmail.com>
2024-03-16 19:48:53 +0330

    Added Specialty Entity, added to Vet. closes #27

--- pet-clinic-data/src/main/java/org/example/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/example/petclinic/model/Vet.java
@@ -2,6 +2,18 @@ package org.example.petclinic.model;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 @Component
 public class Vet extends Person {
+
+    private Set<Specialty> especialties;
+
+    public Set<Specialty> getEspecialties() {
+        return especialties;
+    }
+
+    public void setEspecialties(Set<Specialty> especialties) {
+        this.especialties = especialties;
+    }
 }
