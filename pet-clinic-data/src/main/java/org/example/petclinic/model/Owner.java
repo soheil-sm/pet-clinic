04e6b8d81ad4187830239e4374724f1dc3a07b1d
soheil mahmoodi <soheilmahmoodisr47@gmail.com>
2024-03-16 19:39:05 +0330

    Added Visit Entity, updated Owner with Pet relation. closes #26

--- pet-clinic-data/src/main/java/org/example/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/org/example/petclinic/model/Owner.java
@@ -2,6 +2,18 @@ package org.example.petclinic.model;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 @Component
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
