810a99a1afe2a089ba2ce4fda132368a7ce08e8d
soheil mahmoodi <soheilmahmoodisr47@gmail.com>
2024-03-31 18:04:56 +0330

    Added spring data jpa service for owner. closes #40

--- pet-clinic-data/src/main/java/org/example/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/org/example/petclinic/repositories/OwnerRepository.java
@@ -3,5 +3,9 @@ package org.example.petclinic.repositories;
 import org.example.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+        Owner findByLastName(String lastName);
 }
