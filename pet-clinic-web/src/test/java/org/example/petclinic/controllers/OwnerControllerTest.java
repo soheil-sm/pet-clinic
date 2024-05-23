8fd5cb77077d80ebe29efc7abd84d6daa8014948
soheil mahmoodi <soheilmahmoodisr47@gmail.com>
2024-05-23 12:43:57 +0330

    Implement test for empty find owner lastName. closes #63

--- pet-clinic-web/src/test/java/org/example/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/org/example/petclinic/controllers/OwnerControllerTest.java
@@ -80,6 +80,20 @@ class OwnerControllerTest {
     }
 
     @Test
+    void processFindFormEmptyReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString())).thenReturn(Arrays.asList(
+                Owner.builder().id(5L).build(),
+                Owner.builder().id(6L).build()
+        ));
+
+        mockMvc.perform(get("/owners")
+                .param("lastName", ""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));
+    }
+
+    @Test
     void showOwner() throws Exception {
         when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(2L).build());
 
